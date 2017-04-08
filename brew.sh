e4a864feaa272adc91bfac0283f11bc1f80affd0
Pol Miro <polmiro@gmail.com>
2017-04-08 11:49:12 -0700

    Add brew script

---
+++ brew.sh
@@ -0,0 +1,19 @@
+brew tap caskroom/fonts
+
+brew cask install\
+  spectacle\
+  slack\
+  tmux\
+  iterm2\
+  seil\
+  dropbox\
+  google-chrome\
+  spotify transmission\
+  vlc\
+  caffeine\
+  flux\
+  discord\
+  postico\
+  skype\
+  font-fira-code
+
